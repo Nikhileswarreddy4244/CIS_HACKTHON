2336a83969637c4e225ae2192e0a5448bdf94c9d
Nikhileswarreddy4244 <marthalanikhileswarreddy575@gmail.com>
2026-04-09 12:21:04 +0530

    Configure GitHub Pages deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import UploadPage from './pages/UploadPage';
